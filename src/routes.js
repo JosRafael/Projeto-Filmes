0ef18726013daadc9ba14f9f5d97707942590e8f
JosRafael <rafaelmatiashue@gmail.com>
2024-07-30 00:04:22 -0300

    criando header

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home";
 import Filme from "./pages/filme";
+import Header from "./components/header";
 
 export default function RoutesApp(){
     return(
         <BrowserRouter>
+        <Header/>
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/filme/:id" element={<Filme />}/>
